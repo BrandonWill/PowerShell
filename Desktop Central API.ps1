874d24d1aafdb60d87e277cefe7442cf39c97633
BrandonWill <BrandonWill@users.noreply.github.com>
2019-03-28 21:15:08 -0400

    Added in pagelimit and page to read everything

--- Desktop Central API.ps1
+++ Desktop Central API.ps1
@@ -1,9 +1,32 @@
-$response = Invoke-WebRequest "http://demo.desktopcentral.com/api/1.0/patch/allsystems?=&" -Headers @{"Authorization"="954293BF-AF8D-4D24-9394-DE30C8C72E8E"}
+#https://www.manageengine.com/products/desktop-central/api/gettingstarted.html
 
-$response.Content
+$pagelimit = 25
+$page = 1
+$readMore = $true
+$apiKey = "954293BF-AF8D-4D24-9394-DE30C8C72E8E"
 
-$data = ConvertFrom-Json $response.Content 
+do {
+    
 
-ForEach ($server in $data.message_response.allsystems) {
-    Write-Host "Name: $($server.resource_name) ID: $($server.resource_id) BootTime: $($server.agent_last_bootup_time)"
-}
+    $apiURL = "http://demo.desktopcentral.com/api/1.0/patch/allsystems?=&pagelimit=$($pagelimit)&page=$($page)"
+
+    $response = Invoke-WebRequest $apiURL -Headers @{"Authorization"="$($apiKey)"}
+
+    #$response.Content
+
+    $data = ConvertFrom-Json $response.Content
+
+    Write-Host "------ Page #$($page) ------"
+    ForEach ($server in $data.message_response.allsystems) {
+        Write-Host "Name: $($server.resource_name) ID: $($server.resource_id) BootTime: $($server.agent_last_bootup_time)"
+    }
+
+    $totalRead = $data[0].message_response.limit * $data[0].message_response.page
+
+    if ($totalRead -ige $data[0].message_response.total) {
+        $readMore = $false
+    } else {
+        $page++
+    }
+
+} while ($readMore -eq $true)
